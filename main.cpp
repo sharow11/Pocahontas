3cb77a3f827927ea02e5bb75880da873b2f42065
sharow11 <sharow1234@gmail.com>
2015-10-13 00:27:28 +0200

    Removing std

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@
 #include <string>
 #include <sstream>
 
-std::string convertOutput(mpf_class x, int d)
+using namespace std;
+
+string convertOutput(mpf_class x, int d)
 {
-	std::ostringstream ss;
-    	ss << std::fixed;
-	ss << std::setprecision(d);
+	ostringstream ss;
+    	ss << fixed;
+	ss << setprecision(d);
 	ss << x;
-	std::string str = ss.str();
-	std::string strFormat = str.substr(0, str.find_last_not_of('0') + 1);
+	string str = ss.str();
+	string strFormat = str.substr(0, str.find_last_not_of('0') + 1);
 	size_t dotPos = strFormat.find(".");
 	if(dotPos == strFormat.length() - 1)
 	{
@@ -46,21 +48,21 @@ int main(int argc, char **argv)
 	mpf_class sumPwr;
 	sumPwr = 0;
 
-	std::string strX;
+	string strX;
 
-	while(std::cin >> strX)
+	while(cin >> strX)
 	{
 		x = strX;
 		sum += x;
 		sumPwr += (x * x);
 		n++;
 	}
-	std::cout << n << "\n";
+	cout << n << "\n";
 	mpf_class avg;
 	mpf_class var;
 	avg = sum / n;
-	std::cout << convertOutput(avg, d) << "\n";
+	cout << convertOutput(avg, d) << "\n";
 	var = (sumPwr / n) - avg * avg;
-	std::cout << convertOutput(var, d) << "\n";
+	cout << convertOutput(var, d) << "\n";
 	return 0;
 }
